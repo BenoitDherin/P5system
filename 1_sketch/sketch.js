5c05c5a99cd5933ce28a8e51a268892ec7ecc1ab
Benoit Dherin <benoit.dherin@gmail.com>
2020-04-04 18:08:49 +0200

    added 1_sketch

---
+++ 1_sketch/sketch.js
@@ -0,0 +1,47 @@
+const CRYSTAL_SIZE = 500
+const SIDES = 6
+let PALETTE = []
+
+
+function setup() {
+  createCanvas(530, 530, SVG)
+  
+  PALETTE = [
+    color(255, 52, 154), //pink
+    color(4, 0, 152) //blue
+  ]
+  noLoop()
+  angleMode(DEGREES)
+  // Put the center of the rectangle at the orgin and not the left upper corner
+  rectMode(CENTER)
+}
+
+function draw() {
+  testLines()
+}
+
+function testLines() {
+  const rando = random(1)
+  console.log(rando)
+  let numShapes
+  if (rando > 0.5) {
+    numShapes = SIDES
+  } else {
+    numShapes = SIDES * 2
+  }
+
+
+  noFill()
+  push()
+    translate(width/2, height/2)
+    stroke(PALETTE[0])
+    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
+    stroke(PALETTE[1])
+    const angle = 360 / numShapes
+
+    for (let i = 0; i < numShapes; i++) {
+      line(0, 0, 0, CRYSTAL_SIZE/2)
+      rotate(angle)
+    }
+  pop()
+}
