4204a3f4e54c72158ad129aeea168d710be95f08
Benoit Dherin <benoit.dherin@gmail.com>
2020-04-04 17:25:35 +0200

    first p5 script

--- sketch.js
+++ sketch.js
@@ -1,5 +1,17 @@
 function setup() {
+  createCanvas(500, 500, SVG)
+  noLoop()
+  angleMode(DEGREES)
 }
 
 function draw() {
+  background('teal')
+  fill(0)
+  push()
+    translate(width/2, height/2)
+    rotate(45)
+    rect(0, 0, 25, 25)
+  pop()
+  fill('orange')
+  rect(0, 0, 25, 25)
 }
